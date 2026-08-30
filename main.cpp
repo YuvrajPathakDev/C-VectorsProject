2d357e63f98d02d65dffbc1d4d8802af7e4a7b04
Yuvraj Pathak <pathakyuvraj15@gmail.com>
2026-08-30 04:25:58 +0000

    Normalized Vector and Scalar Multiplication

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ class vector2D{
 private: 
     double Xcoordinate ; 
     double Ycoordinate ; 
+    
 public: 
     vector2D(double xValue , double yValue){
         Xcoordinate = xValue ; 
@@ -31,23 +32,49 @@ public:
         return vector2D(newX, newY);
     }
 
-    double dotProduct (vector2D other){
+    double dotProduct (const vector2D &other)const{
         return Xcoordinate*other.Xcoordinate+Ycoordinate*other.Ycoordinate;
 
     }
     //returning scalar works for the 2D vector because only z-component exists:
-    double crossProduct(vector2D other){
+    double crossProduct(const vector2D &other)const{
         return Xcoordinate*other.Ycoordinate- Ycoordinate*other.Xcoordinate;
+
+    }
+
+    // const after the bracket means that the object that actually uses the function 
+    //cannot be changed by the function. 
+    
+
+    vector2D operator * (double scalar) { 
+        return vector2D(scalar*Xcoordinate, scalar*Ycoordinate); 
+
+    }
+
+    vector2D NormalizeVector()const {
+        
+        double magnitude = std::sqrt(Xcoordinate*Xcoordinate+Ycoordinate*Ycoordinate); 
+        return vector2D(Xcoordinate/magnitude , Ycoordinate/magnitude); 
+
 
     }
 }; 
 
 int main () {
-
+    double scalar= 8.5 ; 
     vector2D v1 (33.1, 11.7); 
     vector2D v2(66.7,93.9) ;
     vector2D v3 = v1+v2 ; 
     vector2D v4 = v1 - v2 ; 
+    vector2D v5 = v1*scalar ; 
+
+    //Unit vector
+    vector2D v6 = v1.NormalizeVector(); 
+
+
+
+
+
     v1.printVector() ; 
     v1.magnitudeVector() ; 
     v2.printVector() ; 
@@ -55,7 +82,9 @@ int main () {
     v3.printVector() ; 
     v4.printVector() ; 
     std::cout <<v1.dotProduct(v2)<<std::endl;
-    std::cout<<v1.crossProduct(v2)<<std::endl; ; 
+    std::cout<<v1.crossProduct(v2)<<std::endl; 
+    v5.printVector() ; 
+    v6.printVector() ; 
 
 
 }
